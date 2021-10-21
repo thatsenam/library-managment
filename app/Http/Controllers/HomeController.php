66e8b7106382f9a25f4bd371274b8e3a30d788ad
Enam <surelastmail@gmail.com>
2021-10-21 22:58:25 +0600

    jd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -19,9 +19,9 @@ class HomeController extends Controller
     public $student_categories_list = array();
 
     public function __construct() {
-        $this->categories_list = Categories::select()->orderBy('category')->get();
-        $this->branch_list = Branch::select()->orderBy('id')->get();
-        $this->student_categories_list = StudentCategories::select()->orderBy('cat_id')->get();
+//        $this->categories_list = Categories::select()->orderBy('category')->get();
+//        $this->branch_list = Branch::select()->orderBy('id')->get();
+//        $this->student_categories_list = StudentCategories::select()->orderBy('cat_id')->get();
     }
 
 	public function home(){
